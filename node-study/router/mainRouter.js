d28ceab5a554268553641201ec915dfb3a4a8e99
kimself <ksy7678@gmail.com>
2021-11-30 22:33:49 +0900

    Add get api

--- node-study/router/mainRouter.js
+++ node-study/router/mainRouter.js
@@ -2,6 +2,8 @@
 const express = require('express') // express 가져오기
 const router = express.Router(); //주소를 만드는 도구를 변수에 담기
 
+const db = require('../model/db');
+
 //주소를 만들 수 있게 도와주는 도구를 담은 변수 router로 주소 생성
 router.get("/", function (req, res) {
 
@@ -29,5 +31,17 @@ router.post("/postapi", function (req, res) {
 })
 
 
+router.get("/data/create", function (req, res) {
+    let user_id = parseInt(Math.random() * 10000) // parseInt: 정수타입으로 만들기, (Math.random(): 랜덤 숫자 생성
+    db.users.create({ user_id: user_id }).then(function (result) {
+        res.send({ success: 200 })
+    }) // db.js의 db에 있는 user_id 컬럼: let user_id
+})
+
+router.get("/data/read", function (req, res) {
+    db.users.findAll().then(function (result) {
+        res.send({ success: 200, data: result })
+    })
+})
 //라우터라는 변수를 밖으로 내보내기
 module.exports = router
